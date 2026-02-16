ff73e518b931255b27028d2d83aaa5c75fed0044
Trifun Savic <trifunsavic@gmail.com>
2026-02-16 10:41:24 +0100

    new config for savag.me

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,8 @@
-const repo = "savag-site";
-const isProd = process.env.NODE_ENV === "production";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
   images: { unoptimized: true },
   trailingSlash: true,
-
-  env: {
-    NEXT_PUBLIC_BASE_PATH: isProd ? `/${repo}` : "",
-  },
-
-  ...(isProd
-    ? {
-        basePath: `/${repo}`,
-        assetPrefix: `/${repo}/`,
-      }
-    : {}),
 };
 
 export default nextConfig;
